805d8eb1b3eb501bf95b9f64cb231968ee2cb0ed
pokemonfan7 <rumblesoso7@gmail.com>
2018-04-07 09:33:42 +0800

    lagou-count

--- myworks/lagoucount/lagoucount.js
+++ myworks/lagoucount/lagoucount.js
@@ -13,7 +13,7 @@
                 // 路径配置
                 require.config({
                     paths: {
-                        echarts: 'http://echarts.baidu.com/build/dist'
+                        echarts: 'echarts.baidu.com/build/dist'
                     }
                 });
 
